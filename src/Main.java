cca888845c39b78a2967dbac1de689bc27c3984f
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-06-15 10:57:15 +0300

    task calculator completed

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    /**
+     * Деление на 0 запрещено.
+     * Создаем конструкцию (try catch), ловим исключение типа ArithmeticException, если пытаемся разделить на 0.
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        Calculator calculator = Calculator.instance.get();
+        int a = calculator.plus.apply(1, 2);
+        int b = calculator.minus.apply(1, 1);
+
+        try {
+            int c = calculator.divide.apply(a, b);
+            calculator.println.accept(c);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage() + ", делить на 0 нельзя");
+        }
+        System.out.println("end!");
+    }
+}
